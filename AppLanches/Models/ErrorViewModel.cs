a745292bd394feab44811049e9d2d20e3ebd711d
Leandro Amaral <lramaral02m@gmail.com>
2024-02-06 12:25:29 -0300

    Adicionar arquivos de projeto.

---
+++ AppLanches/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppLanches.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
