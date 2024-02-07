2cca5726d0ba63893d2b2e01d61c7602fe5ad76b
Leandro Amaral <lramaral02m@gmail.com>
2024-02-07 09:23:33 -0300

    Adicionando migration PopularLanches

---
+++ AppLanches/Migrations/20240206200311_PopularCategorias.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppLanches.Migrations
+{
+    public partial class PopularCategorias : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categorias(CategoriaNome, Descricao)" +
+                "VALUES('Naturais', 'Lanches com igredientes naturais sem carne')");
+
+            migrationBuilder.Sql("INSERT INTO Categorias(CategoriaNome, Descricao)" +
+                "VALUES('Combos', 'Lanches que acompanham batata frita e refrigerante')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categorias");
+        }
+    }
+}
---
+++ AppLanches/Migrations/20240207113745_PopularLanches.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppLanches.Migrations;
+
+public partial class PopularLanches : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql("INSERT INTO Lanches(CategoriaId, " +
+                                "DescricaoCurta, DescricaoDetalhada, EmEstoque," +
+                                "ImagemThumbnailUrl, ImagemUrl, IsLanchePreferido, Nome, Preco)" +
+                                "VALUES(1, 'Pão Integral, Apresuntado, Queijo, Alface, Tomate', 'Um Delecioso lanche leve " +
+                                "para comer a qualquer momento e ingredientes que vão te ajudar na sua dieta', 1,'https://i.pinimg.com/736x/45/f1/d9/45f1d9dc2b8da66345954d4f0c86db9c.jpg', 'https://i.pinimg.com/736x/45/f1/d9/45f1d9dc2b8da66345954d4f0c86db9c.jpg', 0 , 'Lanche Natural', 15.00)");
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql("DELERE FROM Lanches");
+    }
+}
